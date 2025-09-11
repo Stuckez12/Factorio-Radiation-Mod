292002519b64b527fa19f369a1aafd359cd330df
Stuckez12 <jake.stuckey46@gmail.com>
2025-09-11 17:04:14 +0100

    Version 0.10.2
    Additions:
    - Small tag displaying radiation values for every radiative item/fluid/etc...
    Changes:
    - Internal logic on how items are added to radiative lists

--- Stuckez12_Radiation/scripts/mod_integrations.lua
+++ Stuckez12_Radiation/scripts/mod_integrations.lua
@@ -1,57 +1,102 @@
 local mod_addons = {}
 
 
+function Base_Game()
+    log("Base Game - Items Included")
+
+    return {
+        item = {
+            ["uranium-ore"] = {value = 1, type = "item"},
+            ["uranium-238"] = {value = 2, type = "item"},
+            ["uranium-235"] = {value = 5, type = "item"},
+            ["uranium-fuel-cell"] = {value = 10, type = "item"},
+            ["depleted-uranium-fuel-cell"] = {value = 7, type = "item"},
+            ["nuclear-fuel"] = {value = 10, type = "item"},
+            ["uranium-rounds-magazine"] = {value = 2, type = "ammo"},
+            ["uranium-cannon-shell"] = {value = 3, type = "ammo"},
+            ["explosive-uranium-cannon-shell"] = {value = 4, type = "ammo"},
+            ["atomic-bomb"] = {value = 50, type = "ammo"}
+        },
+        fluid = {},
+        unit = {
+            ["big-biter"] = {value = 10, type = "unit"},
+            ["behemoth-biter"] = {value = 50, type = "unit"},
+            ["big-spitter"] = {value = 8, type = "unit"},
+            ["behemoth-spitter"] = {value = 40, type = "unit"}
+        }
+    }
+end
+
+
 function Cerys_Compatible()
-    storage.radiation_items["cerys-radioactive-module-decayed"] = 4
-    storage.radiation_items["cerys-radioactive-module-charged"] = 9
-    storage.radiation_items["plutonium-rounds-magazine"] = 8
-    storage.radiation_items["plutonium-238"] = 7
-    storage.radiation_items["plutonium-239"] = 4
-    storage.radiation_items["plutonium-fuel"] = 14
-    storage.radiation_items["mixed-oxide-fuel-cell"] = 18
-    storage.radiation_items["depleted-mixed-oxide-fuel-cell"] = 8
-    storage.radiation_items["cerys-nuclear-scrap"] = 2
-    storage.radiation_items["cerysian-science-pack"] = 6
-    storage.radiation_items["cerys-neutron-bomb"] = 25
-    storage.radiation_items["cerys-hydrogen-bomb"] = 80
+    log("Cerys Mod - Items Included")
 
-    -- Fluids
-    storage.radiation_fluids["mixed-oxide-waste-solution"] = 1
-
-    log("Cerys Mod - Items Added")
+    return {
+        item = {
+            ["cerys-radioactive-module-decayed"] = {value = 4, type = "module"},
+            ["cerys-radioactive-module-charged"] = {value = 9, type = "module"},
+            ["plutonium-rounds-magazine"] = {value = 8, type = "ammo"},
+            ["plutonium-238"] = {value = 7, type = "item"},
+            ["plutonium-239"] = {value = 4, type = "item"},
+            ["plutonium-fuel"] = {value = 14, type = "item"},
+            ["mixed-oxide-fuel-cell"] = {value = 18, type = "item"},
+            ["depleted-mixed-oxide-fuel-cell"] = {value = 8, type = "item"},
+            ["cerys-nuclear-scrap"] = {value = 2, type = "item"},
+            ["cerysian-science-pack"] = {value = 6, type = "tool"},
+            ["cerys-neutron-bomb"] = {value = 25, type = "ammo"},
+            ["cerys-hydrogen-bomb"] = {value = 80, type = "ammo"}
+        },
+        fluid = {
+            ["mixed-oxide-waste-solution"] = {value = 1, type = "fluid"}
+        },
+        unit = {}
+    }
 end
 
 
 function PlutoniumEnergy_Compatible()
-    storage.radiation_items["plutonium-238"] = 7
-    storage.radiation_items["plutonium-239"] = 4
-    storage.radiation_items["plutonium-fuel-cell"] = 16
-    storage.radiation_items["depleted-plutonium-fuel-cell"] = 10
-    storage.radiation_items["MOX-fuel-cell"] = 14
-    storage.radiation_items["depleted-MOX-fuel-cell"] = 9
-    storage.radiation_items["breeder-fuel-cell"] = 15
-    storage.radiation_items["depleted-breeder-fuel-cell"] = 11
-    storage.radiation_items["plutonium-atomic-artillery-shell"] = 75
-    storage.radiation_items["plutonium-rounds-magazine"] = 8
-    storage.radiation_items["plutonium-cannon-shell"] = 7
-    storage.radiation_items["explosive-plutonium-cannon-shell"] = 8
-    storage.radiation_items["plutonium-fuel"] = 14
+    log("PlutoniumEnergy Mod - Items Included")
 
-    log("PlutoniumEnergy Mod - Items Added")
+    return {
+        item = {
+            ["plutonium-238"] = {value = 7, type = "item"},
+            ["plutonium-239"] = {value = 4, type = "item"},
+            ["plutonium-fuel-cell"] = {value = 16, type = "item"},
+            ["depleted-plutonium-fuel-cell"] = {value = 10, type = "item"},
+            ["MOX-fuel-cell"] = {value = 14, type = "item"},
+            ["depleted-MOX-fuel-cell"] = {value = 9, type = "item"},
+            ["breeder-fuel-cell"] = {value = 15, type = "item"},
+            ["depleted-breeder-fuel-cell"] = {value = 11, type = "item"},
+            ["plutonium-atomic-artillery-shell"] = {value = 75, type = "ammo"},
+            ["plutonium-rounds-magazine"] = {value = 8, type = "ammo"},
+            ["plutonium-cannon-shell"] = {value = 7, type = "ammo"},
+            ["explosive-plutonium-cannon-shell"] = {value = 8, type = "ammo"},
+            ["plutonium-fuel"] = {value = 14, type = "item"}
+        },
+        fluid = {},
+        unit = {}
+    }
 end
 
 
 function Bobs_Warfare_Compatibility()
-    storage.radiation_items["bob-uranium-bullet"] = 2
-    storage.radiation_items["bob-atomic-artillery-shell"] = 60
-    storage.radiation_items["bob-shotgun-uranium-shell"] = 2
-    storage.radiation_items["bob-uranium-bullet-projectile"] = 2
-    
-    log("Bob's Warfare Mod - Items Added")
+    log("Bob's Warfare Mod - Items Included")
+
+    return {
+        item = {
+            ["bob-uranium-bullet"] = {value = 2, type = "item"},
+            ["bob-atomic-artillery-shell"] = {value = 60, type = "ammo"},
+            ["bob-shotgun-uranium-shell"] = {value = 2, type = "ammo"},
+            ["bob-uranium-bullet-projectile"] = {value = 2, type = "item"}
+        },
+        fluid = {},
+        unit = {}
+    }
 end
 
 
-local compatible_mod_funcs = {
+mod_addons.compatible_mod_funcs = {
+    ["base"] = Base_Game,
     ["Cerys-Moon-of-Fulgora"] = Cerys_Compatible,
     ["PlutoniumEnergy"] = PlutoniumEnergy_Compatible,
     ["bobwarfare"] = Bobs_Warfare_Compatibility
@@ -59,31 +104,17 @@ local compatible_mod_funcs = {
 
 
 function mod_addons.integrate_mods()
-    storage.radiation_items = {
-        ["uranium-ore"] = 1,
-        ["uranium-238"] = 2,
-        ["uranium-235"] = 5,
-        ["uranium-fuel-cell"] = 10,
-        ["depleted-uranium-fuel-cell"] = 7,
-        ["nuclear-fuel"] = 10,
-        ["uranium-rounds-magazine"] = 2,
-        ["uranium-cannon-shell"] = 3,
-        ["explosive-uranium-cannon-shell"] = 4,
-        ["atomic-bomb"] = 50
-    }
-
+    storage.radiation_items = {}
     storage.radiation_fluids = {}
-
-    storage.biters = {
-        ["big-biter"] = 10,
-        ["behemoth-biter"] = 50,
-        ["big-spitter"] = 8,
-        ["behemoth-spitter"] = 40
-    }
+    storage.biters = {}
 
     for name, version in pairs(script.active_mods) do
-        if compatible_mod_funcs[name] then
-            compatible_mod_funcs[name]()
+        if mod_addons.compatible_mod_funcs[name] then
+            mod_data = mod_addons.compatible_mod_funcs[name]()
+
+            for name, data in pairs(mod_data.item) do storage.radiation_items[name] = data.value end
+            for name, data in pairs(mod_data.fluid) do storage.radiation_fluids[name] = data.value end
+            for name, data in pairs(mod_data.unit) do storage.biters[name] = data.value end
         end
     end
 
